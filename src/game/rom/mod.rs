f6e99c5e5204a093e9414be4fddebd33f0b40a47
ProDeska <58045784+krystianh4296@users.noreply.github.com>
2026-09-03 22:06:29 +0200

    Export Cartridge from ROM module

--- src/game/rom/mod.rs
+++ src/game/rom/mod.rs
@@ -1,5 +1,7 @@
 pub mod bank;
+pub mod cartridge;
 pub mod reader;
 
 pub use bank::RomBank;
+pub use cartridge::Cartridge;
 pub use reader::{Rom, RomError, RomHeader, EXPECTED_MD5, EXPECTED_ROM_SIZE, ROM_BANK_SIZE};
